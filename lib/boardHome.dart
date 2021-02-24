4436affb944f53ea22945d1208350b78d6257dc8
HSKang <hsuk6032@gmail.com>
2021-02-24 16:42:01 +0900

    editing board

--- lib/boardHome.dart
+++ lib/boardHome.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'freeBoard.dart';
+
 class boardHome extends StatefulWidget {
   @override
   _boardHomeState createState() {
@@ -11,8 +13,29 @@ class _boardHomeState extends State<boardHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Project Locals')),
-      body: Text("this should be Home Page"),
+      appBar: AppBar(title: Text('Board')),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.push_pin_outlined),
+            title: Text('Free Board'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FreeBoard()),
+              );
+            }
+          ),
+          ListTile(
+            leading: Icon(Icons.push_pin_outlined),
+            title: Text('Anonymous Board'),
+          ),
+          ListTile(
+            leading: Icon(Icons.push_pin_outlined),
+            title: Text('Promotion Board'),
+          ),
+        ],
+      ),
     );
   }
 }

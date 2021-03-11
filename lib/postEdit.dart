3c5cf7be4a563bde07422291c98a0a8a909ae0f0
HSKang <hsuk6032@gmail.com>
2021-03-11 17:21:23 +0900

    edit post, delete comment

---
+++ lib/postEdit.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'globals.dart' as globals;
+
+class PostEdit extends StatefulWidget {
+  final DocumentSnapshot post;
+
+  PostEdit({Key key, @required this.post,});
+
+  @override
+  _PostEditState createState() => _PostEditState(key: this.key, post: this.post,);
+}
+
+class _PostEditState extends State<PostEdit> {
+  DocumentSnapshot post;
+
+  _PostEditState({Key key, this.post,});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleController = TextEditingController(text: post['title']);
+    final contentController = TextEditingController(text: post['content']);
+
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        title: Text(
+          '글 수정',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.check,
+            ),
+            onPressed: () async {
+              await post.reference.updateData({
+                'content': contentController.text,
+                'isEdit': true,
+                'title': titleController.text,
+              });
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(15.0, 8.0, 15.0, 8.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 8.0,),
+                TextField(
+                  controller: contentController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
